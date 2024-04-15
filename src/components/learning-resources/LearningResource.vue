3530b9d14a27049f6d8bcee192ccd3b9247f21c7
UJ <sukach.eugene@gmail.com>
2024-04-15 18:02:21 +0300

    resources tab is added

--- src/components/learning-resources/LearningResource.vue
+++ src/components/learning-resources/LearningResource.vue
@@ -1,23 +1,53 @@
 <template>
   <li>
-    <div>
-        <header>
-            <h3>{{ title }}</h3>
-            <button>Delete</button>
-        </header>
-    </div>
-    <p>{{ description}}</p>
-    <nav>
+    <base-card>
+      <header>
+        <h3>{{ title }}</h3>
+        <base-button mode="flat">Delete</base-button>
+      </header>
+
+      <p>{{ description }}</p>
+      <nav>
         <a :href="link">View resource</a>
-    </nav>
+      </nav>
+    </base-card>
   </li>
 </template>
 
 <script>
 export default {
-    props: ['title', 'description', 'link']
+  props: ['title', 'description', 'link'],
 };
 </script>
 
-<style>
+<style scoped>
+li {
+  margin: auto;
+  max-width: 40rem;
+}
+
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+h3 {
+  font-size: 1.25rem;
+  margin: 0.5rem 0;
+}
+
+p {
+  margin: 0.5rem 0;
+}
+
+a {
+  text-decoration: none;
+  color: #ce5c00;
+}
+
+a:hover,
+a:active {
+  color: #c89300;
+}
 </style>
